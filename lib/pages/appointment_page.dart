24fbc28a7ae75e9d829de4a8482ad513127e3dcb
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-27 14:29:38 +0530

    page_build

---
+++ lib/pages/appointment_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: AppointmentPage(),
+  ));
+}
+
+class AppointmentPage extends StatelessWidget {
+  const AppointmentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'APPOINTMENTS',
+          style: GoogleFonts.poppins(
+            color: Colors.black,
+          ),
+        ), // Add this comma
+        centerTitle: true,
+        leading: BackButton(color: Colors.black),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                GFButton(
+                  onPressed: () {},
+                  text: "MY APPOINTMENTS",
+                  icon: Icon(Icons.note_alt_outlined),
+                  type: GFButtonType.outline,
+                  // disabledColor: Colors.blue,
+                ),
+                GFButton(
+                  onPressed: () {},
+                  text: "BOOKED APPOINTMENTS",
+                  icon: Icon(Icons.note_alt_outlined),
+                  type: GFButtonType.outline,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
