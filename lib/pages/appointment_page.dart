d51907b784d6006373ddc66870b3101606da16d7
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-29 12:57:52 +0530

    docx3

--- lib/pages/appointment_page.dart
+++ lib/pages/appointment_page.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../widgets/custom_card.dart';
-import './Appointment Page/dropdown_menu.dart';
 
-const List<String> facilityList = <String>['Facility 1'];
-const List<String> specialtyList = <String>['Specialty 1'];
-const List<String> doctorList = <String>['Doctor 1'];
-const List<String> dateList = <String>['01/01/2023'];
+final List<String> facilityItems = ['Facility 1', 'Facility 2', 'Facility 3'];
+final List<String> specialtyItems = [
+  'Specialty 1',
+  'Specialty 2',
+  'Specialty 3'
+];
+final List<String> doctorItems = ['Doctor 1', 'Doctor 2', 'Doctor 3'];
+final List<String> dateItems = ['01/01/2023', '02/01/2023', '03/01/2023'];
+
+String? selectedFacility;
+String? selectedSpecialty;
+String? selectedDoctor;
+String? selectedDate;
+
+final _formKey = GlobalKey<FormState>();
 
 class AppointmentPage extends StatelessWidget {
   const AppointmentPage({Key? key}) : super(key: key);
@@ -54,16 +64,92 @@ class AppointmentPage extends StatelessWidget {
             ),
           ),
           CustomCard(),
-          const DropdownMenu(list: facilityList),
           const SizedBox(height: 20),
-          const DropdownMenu(list: specialtyList),
-          const SizedBox(height: 20),
-          const DropdownMenu(list: doctorList),
-          const SizedBox(height: 20),
-          const DropdownMenu(list: dateList),
-          const SizedBox(height: 20),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                DropdownMenu(
+                  hint: 'Select Your Facility',
+                  items: facilityItems,
+                  onChanged: (value) => selectedFacility = value,
+                ),
+                const SizedBox(height: 20),
+                DropdownMenu(
+                  hint: 'Select Your Specialty',
+                  items: specialtyItems,
+                  onChanged: (value) => selectedSpecialty = value,
+                ),
+                const SizedBox(height: 20),
+                DropdownMenu(
+                  hint: 'Select Your Doctor',
+                  items: doctorItems,
+                  onChanged: (value) => selectedDoctor = value,
+                ),
+                const SizedBox(height: 20),
+                DropdownMenu(
+                  hint: 'Select Date (DD/MM/YYYY)',
+                  items: dateItems,
+                  onChanged: (value) => selectedDate = value,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      print('Form is valid');
+                    } else {
+                      print('Form is not valid');
+                    }
+                  },
+                  child: const Text('Submit Button'),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class DropdownMenu extends StatelessWidget {
+  final String hint;
+  final List<String> items;
+  final ValueChanged<String?> onChanged;
+
+  DropdownMenu(
+      {required this.hint, required this.items, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<String>(
+      isExpanded: true,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(vertical: 16),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      hint: Text(
+        hint,
+        style: const TextStyle(fontSize: 14),
+      ),
+      items: items
+          .map((item) => DropdownMenuItem<String>(
+              value: item,
+              child: Text(
+                item,
+                style: const TextStyle(fontSize: 14),
+              )))
+          .toList(),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please select an option.';
+        }
+        return null;
+      },
+      onChanged: onChanged,
     );
   }
 }
