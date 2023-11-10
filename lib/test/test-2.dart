f6d01adaf629dd1a42fbb4a25e76a56688980ef5
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-11-10 22:19:02 +0530

    bug fix

--- lib/test/test-2.dart
+++ lib/test/test-2.dart
@@ -1,142 +1,209 @@
-// import 'package:flutter/material.dart';
-// import 'package:getwidget/getwidget.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:baseflow_plugin_template/baseflow_plugin_template.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 
-// import 'doctor_category.dart';
-// import '../../widgets/custom_card.dart';
-// import '../../widgets/custom_dropdown_menu.dart';
-// import '../../widgets/date_picker_widget.dart';
+void main() {
+  CachedNetworkImage.logLevel = CacheManagerLogLevel.debug;
 
-// Category? selectedSpecialty = categories[0];
-// Doctor? selectedDoctor = doctors[0];
-// DateTime selectedDate = DateTime.now();
+  runApp(
+    BaseflowPluginExample(
+      pluginName: 'CachedNetworkImage',
+      githubURL: 'https://github.com/Baseflow/flutter_cached_network_image',
+      pubDevURL: 'https://pub.dev/packages/cached_network_image',
+      pages: [
+        BasicContent.createPage(),
+        ListContent.createPage(),
+        GridContent.createPage(),
+      ],
+    ),
+  );
+}
 
-// final _formKey = GlobalKey<FormState>();
-// class AppointmentPage extends StatefulWidget {
-//  const AppointmentPage({Key? key}) : super(key: key);
+/// Demonstrates a [StatelessWidget] containing [CachedNetworkImage]
+class BasicContent extends StatelessWidget {
+  const BasicContent({super.key});
 
-//  @override
-//  _AppointmentPageState createState() => _AppointmentPageState();
-// }
+  static ExamplePage createPage() {
+    return ExamplePage(Icons.image, (context) => const BasicContent());
+  }
 
-// class _AppointmentPageState extends State<AppointmentPage> {
-//  Category? selectedSpecialty = categories[0];
-//  Doctor? selectedDoctor = doctors[0];
-//  List<Doctor> filteredDoctors = doctors;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _blurHashImage(),
+            _sizedContainer(
+              const Image(
+                image: CachedNetworkImageProvider(
+                  'https://via.placeholder.com/350x150',
+                ),
+              ),
+            ),
+            _sizedContainer(
+              CachedNetworkImage(
+                progressIndicatorBuilder: (context, url, progress) => Center(
+                  child: CircularProgressIndicator(
+                    value: progress.progress,
+                  ),
+                ),
+                imageUrl:
+                    'https://images.unsplash.com/photo-1532264523420-881a47db012d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9',
+              ),
+            ),
+            _sizedContainer(
+              CachedNetworkImage(
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                imageUrl: 'https://via.placeholder.com/200x150',
+              ),
+            ),
+            _sizedContainer(
+              CachedNetworkImage(
+                imageUrl: 'https://via.placeholder.com/300x150',
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.cover,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.red,
+                        BlendMode.colorBurn,
+                      ),
+                    ),
+                  ),
+                ),
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            CachedNetworkImage(
+              imageUrl: 'https://via.placeholder.com/300x300',
+              placeholder: (context, url) => const CircleAvatar(
+                backgroundColor: Colors.amber,
+                radius: 150,
+              ),
+              imageBuilder: (context, image) => CircleAvatar(
+                backgroundImage: image,
+                radius: 150,
+              ),
+            ),
+            _sizedContainer(
+              CachedNetworkImage(
+                imageUrl: 'https://notAvalid.uri',
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            _sizedContainer(
+              CachedNetworkImage(
+                imageUrl: 'not a uri at all',
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            _sizedContainer(
+              CachedNetworkImage(
+                maxHeightDiskCache: 10,
+                imageUrl: 'https://via.placeholder.com/350x200',
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                fadeInDuration: const Duration(seconds: 3),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-//  void onCategoryChanged(Category? category) {
-//    setState(() {
-//      selectedSpecialty = category;
-//      filteredDoctors = doctors
-//          .where((doctor) => doctor.category.id == category?.id)
-//          .toList();
-//      selectedDoctor = filteredDoctors.isNotEmpty ? filteredDoctors[0] : null;
-//    });
-//  }
+  Widget _blurHashImage() {
+    return SizedBox(
+      width: double.infinity,
+      child: CachedNetworkImage(
+        placeholder: (context, url) => const AspectRatio(
+          aspectRatio: 1.6,
+          child: BlurHash(hash: 'LEHV6nWB2yk8pyo0adR*.7kCMdnj'),
+        ),
+        imageUrl: 'https://blurha.sh/assets/images/img1.jpg',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
 
-//  void onDoctorChanged(Doctor? doctor) {
-//    setState(() {
-//      selectedDoctor = doctor;
-//    });
-//  }
+  Widget _sizedContainer(Widget child) {
+    return SizedBox(
+      width: 300,
+      height: 150,
+      child: Center(child: child),
+    );
+  }
+}
 
-//  void onDateSelected(DateTime date) {
-//    setState(() {
-//      selectedDate = date;
-//    });
-//  }
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(
-//          'MEDCARE',
-//          style: GoogleFonts.poppins(
-//            color: Colors.black,
-//          ),
-//        ),
-//        centerTitle: true,
-//        leading: const BackButton(color: Colors.black),
-//        actions: [
-//          IconButton(
-//            icon: Image.asset('assets/logo/medcare.png',
-//                width: 36.0, height: 36.0),
-//            onPressed: () {},
-//          ),
-//        ],
-//        backgroundColor: Colors.transparent,
-//        elevation: 0,
-//      ),
-//      body: SingleChildScrollView(
-//        child: Padding(
-//          padding: const EdgeInsets.all(8.0),
-//          child: Column(
-//            children: <Widget>[
-//              Padding(
-//                padding: const EdgeInsets.all(8.0),
-//                child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: <Widget>[
-//                   GFButton(
-//                     onPressed: () {},
-//                     text: "CREATE APPOINTMENTS",
-//                     type: GFButtonType.outline,
-//                   ),
-//                   GFButton(
-//                     onPressed: () {},
-//                     text: "BOOKED APPOINTMENTS",
-//                     type: GFButtonType.outline,
-//                   ),
-//                 ],
-//                ),
-//              ),
-//              CustomCard(),
-//              const SizedBox(height: 30),
-//              Form(
-//                key: _formKey,
-//                child: Column(
-//                 children: [
-//                   CustomDropdownMenu<Category>(
-//                     hint: 'Select Your Specialty',
-//                     items: categories,
-//                     onChanged: onCategoryChanged,
-//                     value: selectedSpecialty,
-//                     itemBuilder: (Category category) => category.name,
-//                   ),
-//                   const SizedBox(height: 30),
-//                   CustomDropdownMenu<Doctor>(
-//                     hint: 'Select Your Doctor',
-//                     items: filteredDoctors,
-//                     onChanged: onDoctorChanged,
-//                     value: selectedDoctor,
-//                    ```
-//                     itemBuilder: (Doctor doctor) => doctor.name,
-//                   ),
-//                   const SizedBox(height: 30),
-//                   DatePickerWidget(
-//                       onDateSelected: onDateSelected,
-//                       selectedDate: selectedDate),
-//                   const SizedBox(height: 30),
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       if (_formKey.currentState!.validate()) {
-//                         _formKey.currentState!.save();
-//                         print('Form is valid');
-//                       } else {
-//                         print('Form is not valid');
-//                       }
-//                     },
-//                     child: Text('CREATE APPOINTMENTS'),
-//                     style: ElevatedButton.styleFrom(
-//                         shape: RoundedRectangleBorder(
-//                             borderRadius: BorderRadius.circular(20))),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+/// Demonstrates a [ListView] containing [CachedNetworkImage]
+class ListContent extends StatelessWidget {
+  const ListContent({super.key});
+
+  static ExamplePage createPage() {
+    return ExamplePage(Icons.list, (context) => const ListContent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => Card(
+        child: Column(
+          children: <Widget>[
+            CachedNetworkImage(
+              imageUrl: 'https://loremflickr.com/320/240/music?lock=$index',
+              placeholder: (BuildContext context, String url) => Container(
+                width: 320,
+                height: 240,
+                color: Colors.purple,
+              ),
+            ),
+          ],
+        ),
+      ),
+      itemCount: 250,
+    );
+  }
+}
+
+/// Demonstrates a [GridView] containing [CachedNetworkImage]
+class GridContent extends StatelessWidget {
+  const GridContent({super.key});
+
+  static ExamplePage createPage() {
+    return ExamplePage(Icons.grid_on, (context) => const GridContent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 250,
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (BuildContext context, int index) => CachedNetworkImage(
+        imageUrl: 'https://loremflickr.com/100/100/music?lock=$index',
+        placeholder: _loader,
+        errorWidget: _error,
+      ),
+    );
+  }
+
+  Widget _loader(BuildContext context, String url) {
+    return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget _error(BuildContext context, String url, Object error) {
+    return const Center(child: Icon(Icons.error));
+  }
+}
