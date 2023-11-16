0567cfcf0078f95564dd22f33a56fe1bedeb1eef
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-11-17 00:33:30 +0530

    updated tab ui

--- lib/pages/Appointment_Page/appointment_page.dart
+++ lib/pages/Appointment_Page/appointment_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:medcare/widgets/sucess-lottie.dart';
